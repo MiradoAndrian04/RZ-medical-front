3de5f5f029d2e5b8a076d13d6fb89468078ebb97
Fandresenaa <fandresena9torres@gmail.com>
2025-01-13 14:40:42 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import './index.css'
 import AboutComponent from './components/AboutComponent'
+import Produits from './components/Produits'
 
 function App() {
   return (
-    <AboutComponent/>
+    // <AboutComponent/>
+    <Produits/>
   )
 }
 
