dc34c976ddf51c913cee59e1a444713577a134ab
MiradoAndrian04 <miradoandrian25@gmail.com>
2025-01-16 11:40:17 +0300

    modif

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import './App.css'
 import './index.css'
-import Advantages from './components/Advantage'
 
-function App() {
-  return (
-      <Advantages/>
-import ServprodCard from './components/ServprodCard'
 import Navbar from './components/Navbar'
 import HomeCard from './components/HomeCard'
-
+import ServprodCard from './components/ServprodCard'
+import Advantages from './components/Advantage'
+import Footer from './components/page/Footer'
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
 function App() {
   return (
-    // <ServprodCard/>
     <>
-    
     <Navbar/>,
     <HomeCard/>,
-    <ServprodCard/>
+    <ServprodCard/>,
+    <Advantages/>
+    <Footer/>
     </>
   )
 }
