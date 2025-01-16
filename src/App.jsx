5876e4cb486b651f3670f8a8648dec8f2f656105
MiradoAndrian04 <miradoandrian25@gmail.com>
2025-01-16 10:51:43 +0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './App.css'
 import './index.css'
 // import ProduitCard from './components/ProduitCard'
-// import ServprodCard from './components/ServprodCard'
+import ServprodCard from './components/ServprodCard'
 // import ProduitPage from './pages/ProduitPage'
 // import ShowProduit from './pages/ShowProduit'
 import Navbar from './components/Navbar'
 import HomeCard from './components/HomeCard'
-
+import AboutComponent from './components/AboutComponent'
 function App() {
   return (
     // <ServprodCard/>
     <>
     
     <Navbar/>,
-    <HomeCard/>
+    <HomeCard/>,
+    <ServprodCard/>,
+    <AboutComponent/>
     </>
   );
 }
