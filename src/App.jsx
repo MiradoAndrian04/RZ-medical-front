bf68590721503859142da68281569ab7c190e49a
Harizo28 <nyaina280804@gmail.com>
2025-01-16 11:12:25 +0300

    Refactor Advantages component: replace old implementation with animated cards and update App component to use new structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import './App.css'
 import './index.css'
-// import ProduitCard from './components/ProduitCard'
-import ServprodCard from './components/ServprodCard'
-// import ProduitPage from './pages/ProduitPage'
-// import ShowProduit from './pages/ShowProduit'
+import Advantages from './components/Advantage'
 
 function App() {
   return (
-    <ServprodCard/>
-    <div>
-      <Advantage/>
-    </div>
+      <Advantages/>
   )
 }
 
