6f59b33eee537d6b95cb3190bb56a21c81e560d5
MiradoAndrian04 <miradoandrian25@gmail.com>
2025-01-16 11:17:06 +0300

    fontawesome

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import ServprodCard from './components/ServprodCard'
 import Navbar from './components/Navbar'
 import HomeCard from './components/HomeCard'
-import Footer from './pages/Footer'
+import Footer from './components/page/Footer'
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
