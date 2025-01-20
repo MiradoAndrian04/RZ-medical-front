95a41c6c05ffc1f8e37d48db4f756480b6100700
TsinjoRam00 <tsinjoniainaramahandry@gmail.com>
2025-01-20 14:12:32 +0300

    Categorie selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import ContactPage from './components/page/contactpage'
 import Footer from './Footer'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Login from './Admin/login';
+import ProductCategorySelector from './Admin/ProductCategorySelector';
 
 
 function App() {
   return (
     <>
     
-    <Login/>
+    <ProductCategorySelector/>
     </>
   )
 }
