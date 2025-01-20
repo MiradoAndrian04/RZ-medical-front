6dfce7edf770a395342518778664f4b71b3e1624
MiradoAndrian04 <miradoandrian25@gmail.com>
2025-01-20 14:13:02 +0300

    contactMaps

--- src/components/page/ContactPage.jsx
+++ src/components/page/ContactPage.jsx
@@ -21,16 +21,7 @@ function ContactPage() {
             <div className="mt-8">
               <h3 className="text-2xl font-semibold text-gray600">Adresse</h3>
               <p className="text-lg text-gray mt-4 leading-relaxed">
-                <i className="fas fa-map-marker-alt text-red-600"></i> Ankadifotsy, Antananarivo
-              </p>
-            </div>
-
-            {/* Customer Service */}
-            <div className="mt-8">
-              <h3 className="text-2xl font-semibold text-gray600">Service après-vente</h3>
-              <p className="text-lg text-gray mt-4 leading-relaxed">
-                <i className="fas fa-phone-alt text-green-600"></i> +261 34 567 8901<br />
-                <i className="fas fa-envelope text-gray"></i> sav@rzmedical.com
+              <i className="fas fa-map-marker-alt text-red-600"></i> Anosizato, Antananarivo
               </p>
             </div>
           </div>
@@ -39,10 +30,10 @@ function ContactPage() {
           <div className="w-full md:w-3/5">
             <iframe
               className="w-full h-[600px]"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3156905.2140272075!2d43.602645479205226!3d-18.7669475561219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x21f07d4d1a32cbcd%3A0x9e54d7e7e8cba4c2!2sMadagascar!5e0!3m2!1sen!2sus!4v1616679124976!5m2!1sen!2sus
-"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1874!2d47.5078543!3d-18.9368081!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sAnosizato!5e0!3m2!1sfr!2smg!4v1700982401000&t=k" 
+
               allowFullScreen=""
-              loading="lazy"
+              loading="active"
               title="Company Location"
             ></iframe>
           </div>
